1aaf1d9a63c224e98d9cf92a556a2bab16c6941d
NebojsaS1968 <savicnebojsa10@gmail.com>
2021-03-26 15:04:55 +0100

    adding morgan

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema");
+const morgan = require("morgan");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(morgan("dev"));
 
 app.use(
   "/graphql",
