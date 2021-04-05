81d09896a570aae0fe606ed3f2e384346e9f036d
NebojsaS1968 <savicnebojsa10@gmail.com>
2021-04-05 12:13:45 +0200

    cors

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(morgan("dev"));
+app.use(cors());
 
 app.use(
   "/graphql",
