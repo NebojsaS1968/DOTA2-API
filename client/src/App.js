dc88d0f9d423edf79a17b10d938913ac75c04baa
NebojsaS1968 <savicnebojsa10@gmail.com>
2021-04-05 12:14:07 +0200

    displaying hero data from graphql server

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import Heroes from "./components/Heroes";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Heroes</h1>
+      </div>
+      <Heroes />
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/components/Heroes.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+const HEROES_QUERY = gql`
+  query HeroesQuery {
+    heroes {
+      id
+      localized_name
+      roles
+    }
+  }
+`;
+
+const Heroes = () => {
+  const { loading, error, data } = useQuery(HEROES_QUERY);
+
+  if (loading) return <div className="container">Loading...</div>;
+  if (error) return <p>Error :(</p>;
+
+  // Show just the needed roles PROBLEM, not empty strings if there are no more roles available for the current hero !!!!!!!!
+  return data.heroes.map(({ id, localized_name, roles }) => (
+    <div className="hero" key={id}>
+      <p>
+        {localized_name} : Roles({roles[0]}, {roles[1]}, {roles[2]}, {roles[3]},{" "}
+        {roles[4]})
+      </p>
+    </div>
+  ));
+};
+
+export default Heroes;
